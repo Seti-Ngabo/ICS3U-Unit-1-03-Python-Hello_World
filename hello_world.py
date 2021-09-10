d3494737792e441b92a3e3c9206601861476b3d9
Ubuntu <ubuntu@ip-172-31-51-232.ec2.internal>
2021-09-10 17:44:32 +0000

    added file

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#Created by Seti Ngabo
+#Created on September 2021
+#This is the "Hello, World1" program, with proper style
+
+
+def main():
+    print("Hello, World!")
+    
+    
+if __name__ == "__main__":
+    main()
+  
